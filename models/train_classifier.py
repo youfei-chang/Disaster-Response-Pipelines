c6d24333815cc20c81917f99906b74326c555047
Youfei Chang <yxiao@alumni.uwaterloo.ca>
2020-04-27 11:14:15 -0400

    initial commit

---
+++ models/train_classifier.py
@@ -0,0 +1,136 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # ML Pipeline Preparation
+# Follow the instructions below to help you create your ML pipeline.
+# ### 1. Import libraries and load data from database.
+# - Import Python libraries
+# - Load dataset from database with [`read_sql_table`](https://pandas.pydata.org/pandas-docs/stable/generated/pandas.read_sql_table.html)
+# - Define feature and target variables X and Y
+
+# In[15]:
+
+
+# import libraries
+import nltk
+nltk.download(['punkt', 'wordnet'])
+import re
+import numpy as np
+import pandas as pd
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+from sklearn import multioutput
+from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sqlalchemy import create_engine
+
+
+# In[17]:
+
+
+# load data from database
+engine = create_engine('sqlite:///MessageClassification.db')
+df = pd.read_sql_table('data', engine)
+X = df.message.values 
+Y = df.drop(['id', 'message', 'original', 'genre'], axis=1).values
+
+
+# ### 2. Write a tokenization function to process your text data
+
+# In[14]:
+
+
+url_regex = 'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
+def tokenize(text):
+    detected_urls = re.findall(url_regex, text)
+    for url in detected_urls:
+        text = text.replace(url, "urlplaceholder")
+    tokens = word_tokenize(text)
+    lemmatizer = WordNetLemmatizer()
+
+    clean_tokens = []
+    for tok in tokens:
+        clean_tok = lemmatizer.lemmatize(tok).lower().strip()
+        clean_tokens.append(clean_tok)
+
+    return clean_tokens
+
+
+# ### 3. Build a machine learning pipeline
+# This machine pipeline should take in the `message` column as input and output classification results on the other 36 categories in the dataset. You may find the [MultiOutputClassifier](http://scikit-learn.org/stable/modules/generated/sklearn.multioutput.MultiOutputClassifier.html) helpful for predicting multiple target variables.
+
+# In[ ]:
+
+
+pipeline = 
+
+
+# ### 4. Train pipeline
+# - Split data into train and test sets
+# - Train pipeline
+
+# In[ ]:
+
+
+
+
+
+# ### 5. Test your model
+# Report the f1 score, precision and recall for each output category of the dataset. You can do this by iterating through the columns and calling sklearn's `classification_report` on each.
+
+# In[ ]:
+
+
+
+
+
+# ### 6. Improve your model
+# Use grid search to find better parameters. 
+
+# In[ ]:
+
+
+parameters = 
+
+cv = 
+
+
+# ### 7. Test your model
+# Show the accuracy, precision, and recall of the tuned model.  
+# 
+# Since this project focuses on code quality, process, and  pipelines, there is no minimum performance metric needed to pass. However, make sure to fine tune your models for accuracy, precision and recall to make your project stand out - especially for your portfolio!
+
+# In[ ]:
+
+
+
+
+
+# ### 8. Try improving your model further. Here are a few ideas:
+# * try other machine learning algorithms
+# * add other features besides the TF-IDF
+
+# In[ ]:
+
+
+
+
+
+# ### 9. Export your model as a pickle file
+
+# In[ ]:
+
+
+
+
+
+# ### 10. Use this notebook to complete `train.py`
+# Use the template file attached in the Resources folder to write a script that runs the steps above to create a database and export a model based on a new dataset specified by the user.
+
+# In[ ]:
+
+
+
+
